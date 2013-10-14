5dc252eace0f08eee49b7d65c0d4f7bbcafb01c3
Jiankang Wang <jiankang.wang@jago-ag.cn>
2013-10-14 15:48:09 +0800

    Signed-off-by: Jiankang Wang <jiankang.wang@jago-ag.cn>

--- OAuthInterface.php
+++ OAuthInterface.php
@@ -1,6 +1,6 @@
 <?php
 interface OAuthInterface{
-	public function __construct($appid,$appkey);
+	public function __construct($appkey,$appsecret);
 	public function authorize();
 	public function getToken();
 	public function login($token);
